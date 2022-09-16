4312cc00df515a55a7ee282185dbe11dde1e2cfd
Dragos Toma <dragos.toma@siemens-energy.com>
2022-09-16 17:12:34 +0300

    ok

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ app.secret_key = 'super secret key'
 MAX_QUEUE_SIZE = 10 
 MAX_NR_OF_PROCESSES = 4
 
-def worker_function(duration:int,queue):
-    time.sleep(duration)    
-    queue.put(f'worker {multiprocessing.current_process().name} finished in {duration} seconds')
+def worker_function(task_queue,log_queue,stop_queue):
+    while 1:
+        args = task_queue.get()
+        if args is None: break
+        time.sleep(2)    
+        log_queue.put(f'worker {multiprocessing.current_process().name} finished job {args}')
+    stop_queue.put(f'stopping {multiprocessing.current_process().name} end')
      
 @app.route('/tasks_status',methods=['GET'])
 def get_tasks_status(): 
@@ -23,10 +27,8 @@ def get_tasks_status():
 
 @app.route('/run_task',methods=['GET'])
 def run_new_task(): 
-    duration = request.args.get('worker_duration')  
-
     if  _queue.qsize() < MAX_QUEUE_SIZE:
-        _queue.put(int(duration))     
+        _queue.put(request.args.to_dict()['job_name'])     
         if 'jobs' in session:
             session['jobs']+=1
         else:
@@ -43,7 +45,7 @@ def run_new_task():
 
 def listener(task_queue,max_nr_of_processes):
     logging.basicConfig(filename='logs.log',
-                    filemode='a',
+                    filemode='w',
                     format='%(asctime)s, %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO)
@@ -51,50 +53,49 @@ def listener(task_queue,max_nr_of_processes):
 
     processes = {}
     log_queue = multiprocessing.Queue() 
+    stop_queue = multiprocessing.Queue() 
     multiprocessing.log_to_stderr()
     #logger = multiprocessing.get_logger()
     #logger.setLevel(logging.INFO)    
     
     logger.info('starting main process')
-    while 1:                      
-        if not log_queue.empty():
-            m = log_queue.get()          
-            proc_name = re.search('worker\s(.+)\sfinished', m).group(1)
-            processes.pop(proc_name).join()
-            logger.info(m)                      
-        else:
-            if len(processes)<max_nr_of_processes:
-                try:
-                    task = task_queue.get(timeout=5)                  
-                    proc = multiprocessing.Process(target=worker_function, args=(task,log_queue,))                
-                    proc.start()
-                    logger.info(f'starting worker {proc.name}\n')                
-                    processes[proc.name]=proc
-                except Exception as er:
-                    pass
-                    # ignore empty queue error
-                    #f.write(f'Error {str(er)}\n')
-                    #f.flush()
-            else:
-                # Wait a bit to not overrun the main process in case of long lasting processes
-                time.sleep(.1)
+    last_nr_of_tasks = 0 
+    while 1:        
+        request = task_queue.qsize()
         
-        # # safety
-        # if len(processes)==max_nr_of_processes:
-        #     logger.info('cleaning all processes\n')            
-        #     for process in processes.values():
-        #         process.join()
-        #         try:
-        #             m = log_queue.get()
-        #             if m:
-        #                 logger.info(str(m))                        
-        #         except Exception as er:
-        #             #f.write(f'Error {str(er)}\n')
-        #             #f.flush()                        
-        #             pass
-        #     # make sure all processes are joined
-        #     #multiprocessing.active_children()
-        #     processes = {}
+        if request==last_nr_of_tasks:            
+            time.sleep(1)
+        else:
+            last_nr_of_tasks = request
+
+
+        needed_threads = request if request<max_nr_of_processes else max_nr_of_processes
+        existing_threads = len(processes)
+        need_to_stop = existing_threads - needed_threads
+        need_to_raise = needed_threads - existing_threads
+        #logger.info(f'need to stop {need_to_stop} processes {need_to_raise} {existing_threads} {request}')  
+        if need_to_stop>0: 
+            for _ in range(need_to_stop):
+                logger.info(f'need to stop {need_to_stop} processes')    
+                task_queue.put(None)
+                m = stop_queue.get()          
+                proc_name = re.search('stopping\s(.+)\send', m).group(1)
+                processes.pop(proc_name).join()
+                logger.info(m)
+            logger.info(f'current nr of processes {len(processes)}')
+        if need_to_raise>0:  
+            logger.info(f'need to start {need_to_raise} processes')    
+            for _ in range(need_to_raise):
+                proc = multiprocessing.Process(target=worker_function, args=(task_queue,log_queue,stop_queue))                
+                proc.start()
+                logger.info(f'starting worker {proc.name}\n')       
+                processes[proc.name] = proc
+            logger.info(f'current nr of processes {len(processes)}')
+        while not log_queue.empty():                
+            m = log_queue.get()                  
+            logger.info(m)                    
+
+
 
 if __name__ == '__main__':
     
--- test.py
+++ test.py
@@ -15,9 +15,20 @@ import requests
 s = requests.Session()
 URL = f'http://127.0.0.1:5000/run_task'
 URL2 = f'http://127.0.0.1:5000/tasks_status'
-for _ in range(100):
-    rr = s.get(url = URL, params = {'worker_duration':2})
+
+rr = s.get(url = URL, params = {'worker_duration':2,'job_name':'job1'})
+print(rr.json())
+
+time.sleep(4)
+for i in range(1,11):
+    rr = s.get(url = URL, params = {'worker_duration':2,'job_name':f'job{i+1}'})
     print(rr.json())
     time.sleep(.1)
-    rr = s.get(url = URL2)
-    print(rr.json())
+    #rr = s.get(url = URL2)
+    #print(rr.json())
+time.sleep(4)
+rr = s.get(url = URL, params = {'worker_duration':2,'job_name':f'job12'})
+print(rr.json())
+time.sleep(4)
+rr = s.get(url = URL2)
+print(rr.json())
